30b852abaf718c85a9cf91e5a1ae6e5771522c43
Marc <momueller@me.com>
2017-11-04 23:43:17 +0100

    Further Frontend

--- js/connection.js
+++ js/connection.js
@@ -13,17 +13,29 @@ connection.onerror = function(error) {
 	// an error occurred
 	console.log(error);
 };
+
 connection.onclose = function() {
 	// connection to server lost
 	console.log('connection lost');
-}
+};
+
 connection.onmessage = function(message) {
 	// master send data
 	try {
 		var json = JSON.parse(message.data);
 		console.log('received json:', json);
+		if(json.type == 'login') {
+			if(json.successful) {
+				window.link.href = '../homeworkView.html';
+			} else {
+				showError('This is an invalid login. Try again.', 4000);
+			}
+		}
 	} catch (e) {
 		console.log('error when parsing json');
 	}
 };
-connection.send(JSON.stringify({data: 'value'});
+
+function sendUserLogin(username, password) {
+	connection.send(JSON.stringify({type: 'login', username: username, password: password});
+}
